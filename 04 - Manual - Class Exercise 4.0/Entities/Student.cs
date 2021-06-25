bce5cb847e8d8a75a413a9841af3ca0aafad1294
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-25 19:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ 04 - Manual - Class Exercise 4.0/Entities/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Student {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+        public Student(string name, double grade) {
+            Name = name;
+            Grade = grade;
+        }
+        public Student() {
+        }
+
+        public bool Check1() {
+            if(Grade >= 60) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public double Check2() {
+            return (Grade - 60) * (-1);
+        }
+
+        public override string ToString() {
+            if (Check1() == true) {
+                return "Nome do aluno: " + Name + " | Situação do aluno: Aprovado" + " | Média global: " + Grade;
+            }
+            else {
+                return "Nome do aluno: " + Name + "\nSituação do aluno: Reprovado" + "\nMédia global: " + Grade + "\nFaltam " + Check2() + " pontos para aprovação."; 
+            }
+        }
+    }
+}
---
+++ 04 - Manual - Class Exercise 4.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Entities;
+namespace _04___Manual___Class_Exercise_4._0 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("-----------------------------------");
+            Student student = new Student();
+            Console.Write("Digite o nome do aluno: "); student.Name = Console.ReadLine();
+            student.Grade = 0;
+            for(double i = 1; i <= 3; i++) {
+                Console.Write("Digite a nota "+i+ " : ");
+                double sumGrade = double.Parse(Console.ReadLine());
+                student.Grade += sumGrade;
+            }
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(student);
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
